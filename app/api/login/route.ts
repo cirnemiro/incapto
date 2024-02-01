ffbf2780a7da78689f4d368dfa32c0121721a977
enric <cirnemiro@gmail.com>
2024-02-01 17:34:19 +0100

    finished

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -126,6 +126,8 @@ export async function POST(req: NextRequest, res: NextResponse) {
     return NextResponse.json({ error: `provide something` }, { status: 404 })
   }
   const body = await req.json();
+  console.log("");
+  
 
   if (body.email === "clasing@test.com" && body.password === "1234567") {
     return NextResponse.json({ token: `K2OH2FT3W03AQHX565UR40PIEWXXJYJYTRW87GBKZKH3ER3I3L2HFYRPM6PB6TTBJY6TBLZD9IFXD5IRS0BK6MY8WKULWE5XQZEP8T08Z6GYRWXVVBU6PDIZMTCTYHF` }, { status: 200 })
